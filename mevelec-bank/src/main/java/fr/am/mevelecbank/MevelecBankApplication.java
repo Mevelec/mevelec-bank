a6263cf5bece5dcd6bd7d6749aec14c04eca947e
mevelec <adrien.mevelec@hotmail.fr>
2021-05-11 16:29:58 +0200

    preset

---
+++ mevelec-bank/src/main/java/fr/am/mevelecbank/MevelecBankApplication.java
@@ -0,0 +1,13 @@
+package fr.am.mevelecbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MevelecBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MevelecBankApplication.class, args);
+	}
+
+}
---
+++ mevelec-bank/src/test/java/fr/am/mevelecbank/MevelecBankApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.am.mevelecbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MevelecBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
